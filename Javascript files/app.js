22529d4067dda1c3607b1a41de9e99d42eedf7fb
Muhammad Saqib Ali <abc@gmail.com>
2024-08-02 20:13:59 +0500

    Commited the game code

---
+++ Javascript files/app.js
@@ -0,0 +1,59 @@
+const playButton = document.getElementById("play");
+const UI = document.getElementById("UI");
+const score = document.getElementById("score");
+const gameContainer = document.getElementById("game-container");
+const gameArea = document.getElementById("game-area")
+const player = document.getElementById("player");
+const obstacal = document.getElementById("obstacal");
+let gameMode = false;
+
+gameContainer.style.display = "none";
+playButton.addEventListener("click", () => {
+  UI.style.display = "none";
+  gameContainer.style.display = "flex";
+  gameMode = true;
+});
+
+document.addEventListener("keydown", (e) => {
+  if (!gameMode && e.key == "Enter") {
+    UI.style.display = "none";
+    gameContainer.style.display = "flex";
+    gameMode = true;
+  }
+});
+
+let x = 0;
+let y = 0;
+document.addEventListener("keydown", (e) => {
+  if (gameMode && e.key === "ArrowUp") {
+    y = y - 5;
+    player.style.transform = `translate(${x}px,${y}px)`;
+  } else if (gameMode && e.key == "ArrowRight") {
+    x = x + 5;
+    player.style.transform = `translate(${x}px,${y}px)`;
+  } else if (gameMode && e.key == "ArrowDown") {
+    y = y + 5;
+    player.style.transform = `translate(${x}px,${y}px)`;
+  } else if (gameMode && e.key == "ArrowLeft") {
+    x = x - 5;
+    player.style.transform = `translate(${x}px,${y}px)`;
+  }
+});
+
+setInterval(() => {
+    x = x + 2;
+    player.style.transform = `translate(${x}px,${y}px)`;
+}, 25);
+
+
+setTimeout(() => {
+    obstacal.style.transform = "translateX(-1000px)";
+}, 1000);
+
+setInterval(()=>{
+    let hurdle = document.createElement("div");
+    let hurdleId = document.createAttribute("id");
+    hurdleId.value = "obstacal";
+    hurdle.setAttributeNode(hurdleId)
+    gameArea.append(hurdle);
+},250)
